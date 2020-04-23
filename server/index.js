f425226a1b5d91c7576da17fe5ec8d4344943aee
paragpalod <phpalod@gmail.com>
2020-04-24 00:21:53 +0530

    4. configured nodemon created nodemon.json file

--- server/index.js
+++ server/index.js
@@ -2,17 +2,17 @@ const cluster = require('cluster')
 const numCPUs = require('os').cpus().length
 // const express = require('express')
 // const app = express()
-console.log(numCPUs);
+console.log(numCPUs)
 function startServer () {
-    // using all the cpu your machine has to offer to maximize cpu usage and to increase efficiency
-    for (let i = 0; i < numCPUs; i++) {
-      cluster.fork()
-    }
-    cluster.on('online', (worker) => {
-      console.log(`worker is online, Worker Id: ${worker.id}`)
-    })
-    cluster.on('exit', (worker) => {
-      console.log(`worker ${worker.process.pid} died`)
-      cluster.fork()
-    })
+  // using all the cpu your machine has to offer to maximize cpu usage and to increase efficiency
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+  cluster.on('online', (worker) => {
+    console.log(`worker is online, Worker Id: ${worker.id}`)
+  })
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died`)
+    cluster.fork()
+  })
 }
